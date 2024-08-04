c5ab3b8063d5cfdb7101c81d7d9e9fd54f53ea5d
Alson Entuna <alson.entuna@outlook.com>
2024-08-04 15:44:57 +0200

    ! adding environment variable setting whenever the user sets the dev setup

--- dev-setup.bat
+++ dev-setup.bat
@@ -1,12 +1,20 @@
 @echo off
 
+set web_location="C:\dev\website-staging"
+set current_directory=%cd%
+echo "Current directory: %current_directory%"
+
 echo "Checking for staging directory..."
-if not exist "C:\dev\website-staging" (
-	 mkdir "C:\dev\website-staging"
+if not exist %web_location% (
+	 mkdir %web_location%
 	 echo "Staging directory not yet created, creating..."
 ) else (
 	echo "Folder already exists..."
 )
 
-echo "Done"
+echo "Setting environment variable..."
+
+setx DEV_WEBSITE_DIR %current_directory%
+echo "Env variable DEV_WEBSITE_DIR set to: %current_directory%"
+
 pause
