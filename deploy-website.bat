3f4fe22b568195c78b6deec1542512f54e8d8ed4
Alson Entuna <alson.entuna@outlook.com>
2024-08-04 15:52:11 +0200

    * first update for mid-year 2024

--- deploy-website.bat
+++ deploy-website.bat
@@ -1,1 +1,13 @@
-xcopy /exclude:exclusionlist.txt /s /y "D:\Documents\[Files] Web\AlsonEntuna-Portfolio-Build" "D:\Documents\[Files] Web\AlsonEntuna-github.io"
+@echo off
+
+echo Deploying website...
+
+set web_staging_location="C:\dev\website-staging"
+echo Staging location: %web_staging_location%
+
+set web_deployment_location="%DEV_WEBSITE_DIR%"
+echo Deployment location: %web_deployment_location%
+
+xcopy /exclude:exclusionlist.txt /s /y "%web_staging_location%" "%web_deployment_location%"
+
+pause
